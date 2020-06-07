15bee9e20eea9c34d57c3be30a7669bf4251041e
esaenz <esaenz22@gmail.com>
2020-06-07 18:13:26 -0500

    archivo functions.php modificado

--- functions.php
+++ functions.php
@@ -1,5 +1,44 @@
 <?php
 
+	//----//
+	function add_yourtheme_css_js() 
+	{
+
+	    wp_register_style( 'style', get_stylesheet_directory_uri() . '/style.css', array(), '1.1', 'all' );
+
+		wp_register_style( 'font-awesome-min', get_stylesheet_directory_uri() . '/css/fontawesome.css', array(), '5.12.0', 'all' );
+		wp_register_style( 'brands-min', get_stylesheet_directory_uri() . '/css/brands.css', array(), '5.12.0', 'all' );
+		wp_register_style( 'solid-min', get_stylesheet_directory_uri() . '/css/solid.css', array(), '5.12.0', 'all' );
+
+		// owl carousel
+		wp_register_style( 'owl-carousel-min', get_stylesheet_directory_uri() . '/css/owl.carousel.min.css', array(), '2.3.4', 'all' );
+		wp_register_style( 'owl-theme-default-min', get_stylesheet_directory_uri() . '/css/owl.theme.default.min.css', array(), '2.3.4', 'all' );
+
+    	wp_register_script( 'owl-carousel-js', get_template_directory_uri().'/js/owl.carousel.min.js', '', '', true );
+    	wp_register_script( 'app-js', get_template_directory_uri().'/js/app.js', '', '', true );   
+
+    	// css
+		wp_enqueue_style('style');
+		wp_enqueue_style('font-awesome-min');
+		wp_enqueue_style('brands-min');
+		wp_enqueue_style('solid-min');
+		wp_enqueue_style('owl-carousel-min');
+		wp_enqueue_style('owl-theme-default-min');
+
+		// js
+	    wp_enqueue_script('owl-carousel-js');
+	    wp_enqueue_script('app-js');
+
+	}
+	add_action( 'wp_enqueue_scripts', 'add_yourtheme_css_js' );	
+
+	function add_scripts() {
+	        wp_deregister_script('jquery');
+	        wp_register_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js', '', '1.12.4', false);
+	        wp_enqueue_script( 'jquery' );
+	}
+	add_action('wp_enqueue_scripts', 'add_scripts');
+
 	function productosAgrupadosPorCategoria($content = null)
 	{
 		ob_start();
